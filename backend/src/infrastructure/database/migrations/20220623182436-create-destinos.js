badba7736da40fca07194262a5e290ce4fd34355
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-06-23 15:52:18 -0300

    criação da tabela destinos

---
+++ backend/src/infrastructure/database/migrations/20220623182436-create-destinos.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("destinos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome_regiao: {
+        allowNull: false,
+        type: Sequelize.STRING(256)
+      },
+      nome_estado: {
+        allowNull: false,
+        type: Sequelize.STRING(256)
+      },
+      nome_cidade: {
+        allowNull: false,
+        type: Sequelize.STRING(256)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("destinos");
+  }
+};
