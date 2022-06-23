8237000742888ceb12d692d8dedbeccf399b920c
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-06-23 16:14:08 -0300

    criação da tabela comentários

---
+++ backend/src/infrastructure/database/migrations/20220623191207-create-comentarios.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("comentarios", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING(256)
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(256)
+      },
+      mensagem: {
+        allowNull: false,
+        type: Sequelize.STRING(256)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      destinos_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: "destinos"
+          },
+          key: "id"
+        },
+        onDelete: "cascade",
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("comentarios");
+  }
+};
