afca11edf47d95c137dfbfccd5edf0f9f6fc9ca8
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-06-23 15:58:46 -0300

    criação da tabela filtros

---
+++ backend/src/infrastructure/database/migrations/20220623185237-create-filtros.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("filtros", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      palavras_chave: {
+        allowNull: false,
+        type: Sequelize.STRING(400)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("filtros");
+  }
+};
