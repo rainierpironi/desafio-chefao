c0b9b2c04b1f5d09363ec6a625a57ff1cd53fdcc
sabrynnall <limasabrynna03@gmail.com>
2022-06-24 00:09:42 -0300

    criando componente Destinos

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Container from '../../components/Container';
 import Apoie from '../../components/Apoie/Apoie';
 import Sobre from '../../components/Sobre';
+import Destino from '../../components/Destino';
 
 const Home: React.FC = () => {
   return (
     <Container>
         <Sobre />
+        <Destino />
         <Apoie />
     </Container>
   );
