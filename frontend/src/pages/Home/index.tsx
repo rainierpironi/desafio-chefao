56f4db8e63e28ad58e7a7980056dc76e9122744e
unknown <eniofilho76@gmail.com>
2022-06-24 17:52:12 -0300

    Ajustando a landing page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,12 +2,16 @@ import Container from '../../components/Container';
 import Apoie from '../../components/Apoie/Apoie';
 import Sobre from '../../components/Sobre';
 import Destino from '../../components/Destino';
+import Dicas from '../../components/Dicas/Dicas';
+import Conecte from '../../components/Conecte/Conecte';
 
 const Home: React.FC = () => {
   return (
     <Container>
         <Sobre />
         <Destino />
+        <Dicas/>
+        <Conecte/>
         <Apoie />
     </Container>
   );
