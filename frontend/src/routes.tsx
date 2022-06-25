71f72f7f8d5cb5679b4e450885409b7e65b6b166
unknown <eniofilho76@gmail.com>
2022-06-25 16:15:25 -0300

    Modulo quarto da pagina planejamento concluido e resposivo

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,6 +5,7 @@ import Conecte from "./components/Conecte/Conecte";
 import Dicas from "./components/Dicas/Dicas";
 import Plansecond from "./components/Planejamento/Plansecond/Plansecond";
 import Planthird from "./components/Planejamento/Planthird/Planthird";
+import Planfourth from "./components/Planejamento/Planfourth/Planfourth";
 
 
 const MyRoutes: React.FC = () => {
@@ -13,7 +14,7 @@ const MyRoutes: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/plan" element={<Planthird/>} />
+        <Route path="/plan" element={<Planfourth/>} />
       </Routes>
     </Router>
   );
