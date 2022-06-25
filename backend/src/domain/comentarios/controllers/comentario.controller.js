dac358c5bf4ec3f2ac3a97da66507ec721aec966
Ivan Yura Takano <ivanyurat@gmail.com>
2022-06-25 14:44:34 -0300

    criacao dos metodos de buscar por id, atualizar e deletar no controller do comentarios

--- backend/src/domain/comentarios/controllers/comentario.controller.js
+++ backend/src/domain/comentarios/controllers/comentario.controller.js
@@ -1,4 +1,4 @@
-const Comentarios = require('../models');
+const Comentarios = require('../models/Comentarios');
 
 const ComentarioController = {
 
@@ -40,15 +40,54 @@ const ComentarioController = {
     },
 
     async listaComentariosIdDestino(req,res){
-        
+        try {
+            const { id } = req.params;
+            const comentario = await Comentarios.findByPk(id);
+      
+            return res.json(comentario);
+          } catch (error) {
+            return res.status(500).json("Falha ao listar o comentário");
+          }
     },
 
     async atualizaComentario(req,res){
+        try{
+            const {id} = req.params;
+            const {nome, email, mensagem, destinos_id} = req.body;
 
+            const atualizarComentario = await Comentarios.update(
+                {
+                    nome,
+                    email,
+                    mensagem,
+                    destinos_id,
+                },
+                {
+                    where: {
+                        id,
+                    }
+                }
+            );       
+            const ComentarioUpdated = await Comentarios.findByPk(id);
+            return res.status(200).json(ComentarioUpdated);
+        } catch{            
+            return res.status(400).json("Falha ao atualizar os comentários");
+        }     
     },
 
     async deletaComentario(req,res){
-
+        try {
+            const { id } = req.params;
+            await Comentarios.destroy({
+                where: {
+                  id,
+                },
+              });
+        
+              return res.sendStatus(204);
+            } catch (error) {
+              return res.status(500).json("Falha ao deletar o comentário");
+            }
     }
 }
 
