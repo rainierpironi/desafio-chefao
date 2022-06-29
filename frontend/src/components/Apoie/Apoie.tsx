1c8128c23be65f7d4913e8b2caa10317bcae68fd
unknown <eniofilho76@gmail.com>
2022-06-29 15:14:05 -0300

    Pagina apoie pronta

--- frontend/src/components/Apoie/Apoie.tsx
+++ frontend/src/components/Apoie/Apoie.tsx
@@ -10,11 +10,10 @@ function Apoie() {
             <div className='containerApoie'>
             
                 <div className='textoApoie'>
-                <div className='blur-2'></div>
+                
                     <h1>Apoie</h1>
                     <br />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae necessitatibus iure ullam tempore vel voluptatum accusamus quis distinctio, odit quas repellat debitis voluptatem soluta eos? Eum non voluptate doloribus libero deserunt nisi quis dolores reprehenderit adipisci perferendis asperiores officiis, labore nobis impedit tenetur sint optio iste, aspernatur illo. Ipsum omnis porro ab aperiam. Fugit vitae eaque fuga. Et voluptas optio consectetur, quos totam eum ipsa dignissimos distinctio praesentium ducimus vitae est quo deleniti vel, dolor autem veritatis ad repellendus, error velit sapiente pariatur nobis alias. Eveniet consectetur aspernatur ratione repudiandae delectus nisi dolore, omnis, distinctio quas eligendi hic magni?
-                    </p>
+                    <p>Gostou da plataforma? Saiba como você pode contribuir financeiramente para manter essa plataforma funcionando.</p>
                 </div>
 
                 <Figure className='figura' >
