30da62b58127171d712ed746f59c24e2ed251008
Ivan Yura Takano <ivanyurat@gmail.com>
2022-07-02 10:03:30 -0300

    Criacao do DB Jaws no heroku

--- backend/src/infrastructure/database/config/config.js
+++ backend/src/infrastructure/database/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-  console.log("Servidor rodando na porta 4000");
+app.listen(port, () => {
+  console.log("Servidor rodando");
 });
