589431f3b0c662ca5cd23a3dfb9da508cb74cf86
Roberto Chagas <55409496+RobertoChagas@users.noreply.github.com>
2022-07-03 18:23:42 -0300

    Criação do componente FotosCidades e adção deste na página Cidades

---
+++ frontend/src/components/Cidades/FotosCidades/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const FotosCidades: React.FC = () => {
+  return (
+    <div>
+        <h1>Aqui vão as fotos das cidades</h1>
+    </div>
+  );
+}
+
+export default FotosCidades;
