ced76b4289ed7692d8c743fbdb40a33d6cf7052f
unknown <eniofilho76@gmail.com>
2022-07-03 12:57:16 -0300

    pagina dicas culturais pronta falta a responsividade

--- frontend/src/components/Culturais/Culturaissecond/Culturaissecond.tsx
+++ frontend/src/components/Culturais/Culturaissecond/Culturaissecond.tsx
@@ -6,106 +6,162 @@ function Culturaissecond() {
     return (
         <Fragment>
 
-        <div className='containerSecond'>
+            <div className='containerSecond'>
 
-            <div className='borderSecond'></div>
-            <div className='aereasSecond'>Expressões</div>
-            <div className='scrollSecond'>
+                <div className='borderSecond'></div>
+                <div className='aereasSecond'>Expressões</div>
+                <div className='scrollSecond'>
 
-                <Accordion >
-                    <div className='adjustSecond'>
-                        
+                    <Accordion >
+                        <div className='adjustSecond'>
+
                             <Accordion.Item eventKey="0">
-                        
-                        <div className='sombra'>
-                            <Accordion.Header>Norte</Accordion.Header>
+
+                                <div className='sombra'>
+                                    <Accordion.Header>Expressões do Norte</Accordion.Header>
+                                </div>
+                                <Accordion.Body>
+                                    Borogodó – pessoa que se destaca por alguma coisa
+                                    Brocado – estar com fome
+                                    Capar o gato – ir embora
+                                    Curumin – menino, garoto
+                                    De rocha – pessoa que tem palavra, convicção no assunto
+                                    Égua de largura – muita sorte
+                                    Égua – expressa espanto, assim como “eita” ou “caramba”
+                                    Gaiato – uma pessoa que gosta de fazer gracinhas
+                                    Ixi – serve como exclamação
+                                    Levou o farelo – morreu
+                                    Miudinho – pequeno
+                                    Moscou – foi pego em flagrante
+                                    Teú – lagarto
+                                    Tubão – soco na cara
+                                    Vigia bem – presta muita atenção
+                                </Accordion.Body>
+                            </Accordion.Item>
                         </div>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                            est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-            </div>
 
 
-            <div className='adjustSecond'>
-                <Accordion.Item eventKey="1">
-                    <div className='sombra'>
-                        <Accordion.Header>Nordeste</Accordion.Header>
-                    </div>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-            </div>
+                        <div className='adjustSecond'>
+                            <Accordion.Item eventKey="1">
+                                <div className='sombra'>
+                                    <Accordion.Header>Expressões do Nordeste</Accordion.Header>
+                                </div>
+                                <Accordion.Body>
+                                    Abestado –  bobo, tolo, leso
+                                    Acoitar – proteger
+                                    Aluado – que fica no mundo da lua
+                                    Amarrado – pessoa avarenta, mesquinha
+                                    Amofinado –  aborrecido
+                                    Aperrear – encher o saco
+                                    Apombaiado – nunca entende nada
+                                    Arretado – que é legal, bacana, tudo de bom
+                                    Avexado – que está apressado
+                                    Azuretado –  confuso
+                                    Barril – quando algo é difícil ou perigoso
+                                    Bizonho – pessoa quando triste, calado
+                                    Buchuda – gestante
+                                    Cafuringa – coisa muito pequena
+                                    Desmantelar – arruinar
+                                    Estrambólico – esquisito, extravagante
+                                    Farrapar – marcar um compromisso e faltar
+                                    Fuzuê – confusão, barulho
+                                    Macambúzio – pensativo, tristonho, cabisbaixo
+                                    Mangar – fazer graça com alguém
+                                    Migué – falar algo só para convencer alguém
+                                    Morgado – desanimado
+                                    Pagar sapo – passar vergonha
+                                    Sustança – comida que dá força e vigor
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </div>
 
 
-            <div className='adjustPlan'>
-                <Accordion.Item eventKey="2">
-                    <div className='sombra'>
-                        <Accordion.Header>Centro-Oeste</Accordion.Header>
-                    </div>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-            </div>
-            <div className='adjustPlan'>
-                <Accordion.Item eventKey="3">
-                    <div className='sombra'>
-                        <Accordion.Header>Sudeste</Accordion.Header>
-                    </div>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-            </div>
-            <div className='adjustPlan'>
+                        <div className='adjustPlan'>
+                            <Accordion.Item eventKey="2">
+                                <div className='sombra'>
+                                    <Accordion.Header>Expressões do Centro-Oeste</Accordion.Header>
+                                </div>
+                                <Accordion.Body>
+                                    Abiscoitar – receber dinheiro ou herança
+                                    Arruinou – piorou de saúde
+                                    Bereré – propina, dinheiro ganhado fácil
+                                    Bitelo – algo grande
+                                    Borracho – bêbado
+                                    Camela – bicicleta
+                                    Carreta – carro
+                                    Comer na gaveta – se relaciona a uma pessoa que é avarenta, um pão-duro
+                                    Descabriado – descontrolado
+                                    Dormir no macio – viver folgado
+                                    Empatar – atrapalhar
+                                    Madurar – amadurecer
+                                    Não dá conta – não consegue fazer algo que precisa ser feito
+                                    Para de mula – perturbar
+                                    Patife – alguém medroso
+                                    Pé-de-boi – pessoa que é trabalhadora
+                                    Perrengue – pessoa está adoentada ou enfraquecida
+                                    Tá chovendo duro – chover torrencialmente
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </div>
+                        <div className='adjustPlan'>
+                            <Accordion.Item eventKey="3">
+                                <div className='sombra'>
+                                    <Accordion.Header>Expressões doSudeste</Accordion.Header>
+                                </div>
+                                <Accordion.Body>
+                                    Bater um rango – comer, matar a fome
+                                    Bolado – preocupado
+                                    Chapar o coco – beber muito
+                                    Da hora – legal
+                                    É fria – é perigoso
+                                    Jacú – bobo, ignorante
+                                    Larica – fome
+                                    Marombado – pessoa que faz musculação, quem é musculoso
+                                    Padoca – padaria
+                                    Quebrado – sem dinheiro
+                                    Rolê – sair, dar uma volta, passeio
+                                    Sangue bom – pessoa legal, gente fina
+                                    Só o pó – cansado
+                                    Trem – qualquer coisa, qualquer objeto
+                                    Treta – confusão, briga
+                                    Trocar ideia – conversar
+                                    Zueira – brincadeira, bagunça
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </div>
+                        <div className='adjustPlan'>
 
-                <Accordion.Item eventKey="4">
-                    <div className='sombra'>
-                        <Accordion.Header>Sul</Accordion.Header>
-                    </div>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-            </div>
-        </Accordion>
-    </div>
-        </div >
+                            <Accordion.Item eventKey="4">
+                                <div className='sombra'>
+                                    <Accordion.Header>Expressões do Sul</Accordion.Header>
+                                </div>
+                                <Accordion.Body>
+                                    Alçar a pena – montar no cavalo
+                                    Bah – é uma expressão usada em diversas situações e no início das frases
+                                    Cacetinho – pão francês
+                                    Cair os butiá do bolso – se assustar, ficar surpreso com algo
+                                    Dar uma banda – sair, dar uma volta
+                                    Deitar o cabelo – fugir, cair fora
+                                    Desabotinado – adoidado, estourado
+                                    Embretar-se – meter-se em apuros
+                                    Esgualepado – ferido, machucado
+                                    Guaipeca – cachorro, vira-lata
+                                    Guri e Guria – menino e menina
+                                    Lagartear – ficar no sol
+                                    Lindeiro – vizinho
+                                    Solito – sozinho, sem companhia
+                                    Tchê – usado geralmente no fim das frases para chamar atenção
+                                    Tri – bom, legal, muito maneiro
+                                    Trovar – chavecar
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </div>
+                    </Accordion>
+                </div>
+            </div >
 
-    </Fragment >
-    )      
+        </Fragment >
+    )
 
 }
 
