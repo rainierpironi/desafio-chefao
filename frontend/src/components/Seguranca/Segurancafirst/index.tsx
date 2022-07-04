bf9102ae4bf0b05e0d569e032b6d86aa845aa0a6
unknown <eniofilho76@gmail.com>
2022-07-03 22:32:52 -0300

    pagina seguranca feita , falta responsividade

--- frontend/src/components/Seguranca/Segurancafirst/index.tsx
+++ frontend/src/components/Seguranca/Segurancafirst/index.tsx
@@ -1,31 +1,29 @@
 import React, { Fragment } from 'react'
 import { Figure } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import Imagem from "../../../assets/Images/seguranca.jpg"
+import Imagem from "../../../assets/Images/segdesktop.png"
 import "../Segurancafirst/styles.css"
+import Faixa from "../../../assets/Images/segfaixa.png"
 
 function SecurityFirst() {
   return (
     <Fragment>
+<main className='containercult'>
+        
+        <div className='containerBack'>
+          <img src={Imagem} alt="" />
+        </div>
+        
+        <div className='titulocult'>
+          <h1>Dicas Culturais</h1>
+          <p>Conhecendo a riqueza das diferenças culturais no Brasil...</p>
+        </div>
+        <div className='faixacult'>
+          <img src={Faixa} alt="" />
+        </div>
 
-<div className='containerSecurityFirst'>
-                <div className='textoSecurityFirst'>
-                    <p>Identificando fatores de risco para evitá-los...</p>
-                    <br />
-                    <h1>Dicas</h1>
-                    <h6>Seguranca</h6>
-                </div>
+      </main>
 
-                <Figure className='figuraSecurityFirst' >
-                    <Link to="/seguranca">
-                    <Figure.Image  alt="Imagem Seguranca" src='' />
-                    </Link>
-                    
-                </Figure>
-                
-            </div>
-
-            
 
 
     </Fragment>
