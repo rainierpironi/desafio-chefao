2216d678abe929f683850e8ddee56960534c8dfa
unknown <eniofilho76@gmail.com>
2022-07-05 16:00:06 -0300

    Ajuste do texto no dropdown da pagina seguranca

--- frontend/src/components/Seguranca/Segurancasecond/index.tsx
+++ frontend/src/components/Seguranca/Segurancasecond/index.tsx
@@ -4,68 +4,36 @@ import "../Segurancasecond/styles.css"
 
 function SecuritySecond() {
     return (
-        
+
         <Fragment>
 
-            
-<div className='containerSecond'>
 
-<div className='borderSecond'></div>
-<div className='aereasSecond'>Precauções</div>
-<div className='scrollSecond'>
+            <div className='containerSecond'>
 
-    <Accordion >
-        <div className='adjustSecond'>
+                <div className='borderSecond'></div>
+                <div className='aereasSecond'>Precauções</div>
+                <div className='scrollSecond'>
 
-            <Accordion.Item eventKey="0">
+                    <Accordion >
+                        <div className='adjustSecond'>
 
-                <div className='sombra'>
-                    <Accordion.Header>Cuidados essenciais</Accordion.Header>
+                            <Accordion.Item eventKey="0">
+
+                                <div className='sombra'>
+                                    <Accordion.Header>Cuidados essenciais</Accordion.Header>
+                                </div>
+                                <Accordion.Body>
+                                    <b>Evite desembarcar tarde da noite</b> atentando ao horário de desembarque no destino escolhido ao comprar sua passagem.
+                                    <b>Fique atenta ao pegar táxi ou transporte público</b> já que não é raro um taxista ou motorista de aplicativo fazer um caminho mais longo ao notar que o cliente não conhece a cidade. Tenha um mapa e confira o percurso antes de entrar no veículo.
+                                    <b>Escolha bem a acomodação</b> atentando sobre a localização e às avaliações. ,<b>Deixe uma cópia do seu roteiro com alguém da família ou amigos</b> além de manter contato diariamente, caso aconteça algo, as pessoas poderão ajudar, ou dar informações sobre a sua viagem ou chamando a emergência. <b>Evite caminhar à noite sozinha </b>por mais que pegar um táxi ou o transporte público saia mais caro, é melhor do que perder seus documentos e pertences em um lugar desconhecido. <b>Procure passeios em grupo</b> você pode fazer algumas amizades. <b>Não exagere no álcool </b>ele pode alterar a sua visão e a sua percepção tornando-a mais suscetível a roubo. <b>Leve seus mecanismos de defesa portátil para todo lugar</b> contra roubos ou abusadores. Spray de pimenta ou algo que possa te ajudar a se defender, como um chaveiro pontudo por exemplo.<b> Não saia sem seu telefone </b>ele é um item fundamental. Leve seu carregador na bolsa, confira o nível de bateria ao sair e tenha sempre na bolsa o endereço e o telefone do seu hotel. <b>Tenha todos os documentos essenciais para viajar sozinha</b> – RG, CNH ou outro documento de identificação pessoal oficial para viagem em território nacional, além do comprovante de vacinação contra o COVID-19. <b>Cuide da sua alimentação </b>procurando conhecer detalhes da gastronomia do seu destino, assim, evitando dores de cabeça futuras. O seguro viagem, mesmo em viagem nacional, é imprescindível para que se algo acontecer com você ou com sua bagagem, o atendimento esteja garantido.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </div>
+
+
+                    </Accordion>
                 </div>
-                <Accordion.Body>
-                Evite desembarcar tarde da noite
-Quando for comprar sua passagem, fique atenta ao horário de desembarque no destino escolhido. Desembarcar sozinha à noite em um lugar desconhecido tem seus perigos. Reserve um voo que chegue ao seu destino durante o horário comercial.
-
-Mantenha contato com amigos e familiares
-Viajar sozinha apresenta vários benefícios, mas também tem seus riscos. Por isso, é importante manter contato e fornecer informações atualizadas para amigos ou familiares. Prepare um documento com detalhes de seus roteiros e compartilhe com pessoas próximas. 
-
-Fique atenta ao pegar táxi ou transporte público
-Não é raro um taxista ou motorista de aplicativo fazer um caminho mais longo ao notar que o cliente não conhece a cidade. Por isso, tenha sempre um mapa em mãos e confira o percurso antes de entrar no veículo. 
-
-Escolha bem a acomodação
-Ao escolher o local onde se hospedar é preciso ter alguns cuidados. Procure acomodações que tenham boas avaliações e localização. Se seu orçamento permitir, escolha um quarto apenas seu, que tenha chave na porta. 
-
-Deixe uma cópia do seu roteiro com alguém da família ou amigos
-Além de manter contato diariamente, é fundamental deixar uma cópia do seu roteiro de viagem com alguém da sua confiança. Pois, caso aconteça algo, a pessoa poderá ajudar, ou dando informações sobre a sua viagem ou chamando a emergência. 
-
-Evite caminhar à noite sozinha
-Por mais que pegar um táxi ou o transporte público saia mais caro, é melhor do que perder seus documentos e pertences em um lugar desconhecido. Lembrando que não estamos falando para você não sair a noite! Longe disso! O ideal é sair mesmo. 
-
-Procure passeios em grupo
-Bom, viajar sozinha tem a ver com independência, mas procurar alguns passeios em grupos pode ser bem legal. Você pode fazer algumas amizades e além disso, dar umas risadas e conhecer lugares magníficos com pessoas novas. 
-
-Não exagere no álcool
-Quando se viaja sozinha, experimentar novas bebidas faz parte do roteiro. Só não exagere nas doses! Pois o álcool pode alterar a visão e percepção e você estará mais suscetível a um roubo, por exemplo. Por isso, não ultrapasse o seu limite.
-
-Leve seus mecanismos de defesa portátil para todo lugar
-É importante carregar na bolsa suas ‘armas’ contra roubos ou abusadores. E, se esses produtos, como o spray de pimenta forem proibidos, sempre carregue algo que possa te dar uma vantagem, como chaveiros pontudos e outros que possam te ajudar a se defender.
-
-Não saia sem seu telefone
-O seu telefone é um item importantíssimo quando se viaja, sozinha ou não. Sempre leve seu carregador na bolsa e ao sair de casa e confira o nível de bateria também. Outra opção é ter na bolsa, não só o endereço dos locais, como o telefone do seu hotel. 
-
-Tenha todos os documentos essenciais para viajar sozinha
-Para viagens dentro do Brasil, não é necessário passaporte. Mas você deve levar um RG, CNH ou outro documento de identificação pessoal oficial. Outro documento essencial é o comprovante de vacinação contra o COVID-19. 
-
-Cuide da sua alimentação
-Saber esses detalhes sobre a alimentação do seu destino pode evitar dores de cabeça futuras. Não esqueça o seguro viagem! Mesmo em viagem nacional, o seguro viagem é imprescindível. Por meio dele que, se algo acontecer com você ou com a sua bagagem, o atendimento está garantido. 
-                </Accordion.Body>
-            </Accordion.Item>
-        </div>
-
-        
-    </Accordion>
-</div>
-</div >
+            </div >
 
 
         </Fragment >
