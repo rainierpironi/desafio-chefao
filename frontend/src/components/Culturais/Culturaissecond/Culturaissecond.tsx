100449fa2bbde1d40784ea7d4b4127c4c3ab03aa
unknown <eniofilho76@gmail.com>
2022-07-05 14:03:15 -0300

    ajuste de texto na pagina culturais

--- frontend/src/components/Culturais/Culturaissecond/Culturaissecond.tsx
+++ frontend/src/components/Culturais/Culturaissecond/Culturaissecond.tsx
@@ -21,21 +21,21 @@ function Culturaissecond() {
                                     <Accordion.Header>Expressões do Norte</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Borogodó – pessoa que se destaca por alguma coisa
-                                    Brocado – estar com fome
-                                    Capar o gato – ir embora
-                                    Curumin – menino, garoto
-                                    De rocha – pessoa que tem palavra, convicção no assunto
-                                    Égua de largura – muita sorte
-                                    Égua – expressa espanto, assim como “eita” ou “caramba”
-                                    Gaiato – uma pessoa que gosta de fazer gracinhas
-                                    Ixi – serve como exclamação
-                                    Levou o farelo – morreu
-                                    Miudinho – pequeno
-                                    Moscou – foi pego em flagrante
-                                    Teú – lagarto
-                                    Tubão – soco na cara
-                                    Vigia bem – presta muita atenção
+                                    <b> Borogodó</b> – pessoa que se destaca por alguma coisa.
+                                    <b>Brocado</b> – estar com fome.
+                                    <b> Capar o gato</b> – ir embora.
+                                    <b>Curumin</b> – menino, garoto.
+                                    <b>De rocha</b> – pessoa que tem palavra, convicção no assunto.
+                                    <b>Égua de largura</b> – muita sorte.
+                                    <b>Égua</b> – expressa espanto, assim como “eita” ou “caramba”.
+                                    <b>Gaiato</b> – uma pessoa que gosta de fazer gracinhas.
+                                    <b>Ixi</b> – serve como exclamação.
+                                    <b>Levou o farelo</b> – morreu.
+                                    <b>Miudinho</b> – pequeno.
+                                    <b>Moscou</b> – foi pego em flagrante.
+                                    <b>Teú</b> – lagarto.
+                                    <b>Tubão</b> – soco na cara.
+                                    <b>Vigia bem</b> – presta muita atenção.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
@@ -47,30 +47,30 @@ function Culturaissecond() {
                                     <Accordion.Header>Expressões do Nordeste</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Abestado –  bobo, tolo, leso
-                                    Acoitar – proteger
-                                    Aluado – que fica no mundo da lua
-                                    Amarrado – pessoa avarenta, mesquinha
-                                    Amofinado –  aborrecido
-                                    Aperrear – encher o saco
-                                    Apombaiado – nunca entende nada
-                                    Arretado – que é legal, bacana, tudo de bom
-                                    Avexado – que está apressado
-                                    Azuretado –  confuso
-                                    Barril – quando algo é difícil ou perigoso
-                                    Bizonho – pessoa quando triste, calado
-                                    Buchuda – gestante
-                                    Cafuringa – coisa muito pequena
-                                    Desmantelar – arruinar
-                                    Estrambólico – esquisito, extravagante
-                                    Farrapar – marcar um compromisso e faltar
-                                    Fuzuê – confusão, barulho
-                                    Macambúzio – pensativo, tristonho, cabisbaixo
-                                    Mangar – fazer graça com alguém
-                                    Migué – falar algo só para convencer alguém
-                                    Morgado – desanimado
-                                    Pagar sapo – passar vergonha
-                                    Sustança – comida que dá força e vigor
+                                    <b>Abestado</b> –  bobo, tolo, leso.
+                                    <b>Acoitar</b> – proteger.
+                                    <b>Aluado</b> – que fica no mundo da lua.
+                                    <b>Amarrado</b> – pessoa avarenta, mesquinha.
+                                    <b>Amofinado</b> –  aborrecido.
+                                    <b>Aperrear</b> – encher o saco.
+                                    <b>Apombaiado</b> – nunca entende nada.
+                                    <b>Arretado</b> – que é legal, bacana, tudo de bom.
+                                    <b>Avexado</b> – que está apressado.
+                                    <b>Azuretado</b> –  confuso.
+                                    <b>Barril</b> – quando algo é difícil ou perigoso.
+                                    <b>Bizonho</b> – pessoa quando triste, calado.
+                                   <b> Buchuda</b> – gestante.
+                                   <b>Cafuringa</b> – coisa muito pequena.
+                                   <b>Desmantelar</b> – arruinar.
+                                   <b>Estrambólico</b> – esquisito, extravagante.
+                                   <b>Farrapar</b> – marcar um compromisso e faltar.
+                                   <b>Fuzuê</b> – confusão, barulho.
+                                   <b>Macambúzio</b> – pensativo, tristonho, cabisbaixo.
+                                   <b>Mangar</b> – fazer graça com alguém.
+                                   <b>Migué</b> – falar algo só para convencer alguém.
+                                   <b>Morgado</b> – desanimado.
+                                   <b>Pagar sapo</b> – passar vergonha.
+                                   <b>Sustança</b> – comida que dá força e vigor.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
@@ -82,24 +82,24 @@ function Culturaissecond() {
                                     <Accordion.Header>Expressões do Centro-Oeste</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Abiscoitar – receber dinheiro ou herança
-                                    Arruinou – piorou de saúde
-                                    Bereré – propina, dinheiro ganhado fácil
-                                    Bitelo – algo grande
-                                    Borracho – bêbado
-                                    Camela – bicicleta
-                                    Carreta – carro
-                                    Comer na gaveta – se relaciona a uma pessoa que é avarenta, um pão-duro
-                                    Descabriado – descontrolado
-                                    Dormir no macio – viver folgado
-                                    Empatar – atrapalhar
-                                    Madurar – amadurecer
-                                    Não dá conta – não consegue fazer algo que precisa ser feito
-                                    Para de mula – perturbar
-                                    Patife – alguém medroso
-                                    Pé-de-boi – pessoa que é trabalhadora
-                                    Perrengue – pessoa está adoentada ou enfraquecida
-                                    Tá chovendo duro – chover torrencialmente
+                                    <b>Abiscoitar</b> – receber dinheiro ou herança.
+                                    <b>Arruinou</b> – piorou de saúde.
+                                    <b>Bereré</b> – propina, dinheiro ganhado fácil.
+                                    <b>Bitelo</b> – algo grande.
+                                    <b>Borracho</b> – bêbado.
+                                    <b>Camela</b> – bicicleta.
+                                    <b>Carreta</b>  – carro.
+                                    <b>Comer na gaveta</b> – se relaciona a uma pessoa que é avarenta, um pão-duro.
+                                    <b>Descabriado</b> – descontrolado.
+                                    <b>Dormir no macio</b> – viver folgado.
+                                    <b>Empatar</b> – atrapalhar.
+                                    <b>Madurar</b> – amadurecer.
+                                    <b>Não dá conta</b> – não consegue fazer algo que precisa ser feito.
+                                    <b>Para de mula</b> – perturbar.
+                                    <b>Patife</b> – alguém medroso.
+                                    <b>Pé-de-boi</b> – pessoa que é trabalhadora.
+                                    <b>Perrengue</b> – pessoa está adoentada ou enfraquecida.
+                                    <b>Tá chovendo duro</b> – chover torrencialmente.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
@@ -109,23 +109,23 @@ function Culturaissecond() {
                                     <Accordion.Header>Expressões doSudeste</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Bater um rango – comer, matar a fome
-                                    Bolado – preocupado
-                                    Chapar o coco – beber muito
-                                    Da hora – legal
-                                    É fria – é perigoso
-                                    Jacú – bobo, ignorante
-                                    Larica – fome
-                                    Marombado – pessoa que faz musculação, quem é musculoso
-                                    Padoca – padaria
-                                    Quebrado – sem dinheiro
-                                    Rolê – sair, dar uma volta, passeio
-                                    Sangue bom – pessoa legal, gente fina
-                                    Só o pó – cansado
-                                    Trem – qualquer coisa, qualquer objeto
-                                    Treta – confusão, briga
-                                    Trocar ideia – conversar
-                                    Zueira – brincadeira, bagunça
+                                    <b>Bater um rango</b> – comer, matar a fome.
+                                    <b>Bolado</b> – preocupado.
+                                    <b>Chapar o coco</b> – beber muito.
+                                    <b>Da hora</b> – legal.
+                                    <b>É fria</b> – é perigoso.
+                                    <b>Jacú</b> – bobo, ignorante.
+                                    <b>Larica</b> – fome.
+                                    <b>Marombado</b> – pessoa que faz musculação, quem é musculoso.
+                                    <b>Padoca</b> – padaria.
+                                    <b>Quebrado</b> – sem dinheiro.
+                                    <b>Rolê</b> – sair, dar uma volta, passeio.
+                                    <b>Sangue bom</b> – pessoa legal, gente fina.
+                                    <b>Só o pó</b> – cansado.
+                                    <b>Trem – qualquer</b> coisa, qualquer objeto.
+                                    <b>Treta</b> – confusão, briga.
+                                    <b>Trocar ideia</b> – conversar.
+                                    <b>Zueira</b> – brincadeira, bagunça.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
@@ -136,23 +136,23 @@ function Culturaissecond() {
                                     <Accordion.Header>Expressões do Sul</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Alçar a pena – montar no cavalo
-                                    Bah – é uma expressão usada em diversas situações e no início das frases
-                                    Cacetinho – pão francês
-                                    Cair os butiá do bolso – se assustar, ficar surpreso com algo
-                                    Dar uma banda – sair, dar uma volta
-                                    Deitar o cabelo – fugir, cair fora
-                                    Desabotinado – adoidado, estourado
-                                    Embretar-se – meter-se em apuros
-                                    Esgualepado – ferido, machucado
-                                    Guaipeca – cachorro, vira-lata
-                                    Guri e Guria – menino e menina
-                                    Lagartear – ficar no sol
-                                    Lindeiro – vizinho
-                                    Solito – sozinho, sem companhia
-                                    Tchê – usado geralmente no fim das frases para chamar atenção
-                                    Tri – bom, legal, muito maneiro
-                                    Trovar – chavecar
+                                <b>Alçar a pena</b> – montar no cavalo.
+                                <b>Bah</b> – é uma expressão usada em diversas situações e no início das frases.
+                                <b>Cacetinho</b> – pão francês.
+                                <b>Cair os butiá do bolso</b> – se assustar, ficar surpreso com algo.
+                                <b>Dar uma banda</b> – sair, dar uma volta.
+                                <b>Deitar o cabelo</b> – fugir, cair fora.
+                                <b>Desabotinado</b> – adoidado, estourado.
+                                <b>Embretar-se</b> – meter-se em apuros.
+                                <b>Esgualepado</b> – ferido, machucado.
+                                <b>Guaipeca</b> – cachorro, vira-lata.
+                                <b>Guri e Guria</b> – menino e menina.
+                                <b>Lagartear</b> – ficar no sol.
+                                <b>Lindeiro</b> – vizinho.
+                                <b>Solito</b> – sozinho, sem companhia.
+                                <b>Tchê</b> – usado geralmente no fim das frases para chamar atenção.
+                                <b>Tri</b> – bom, legal, muito maneiro.
+                                <b>Trovar</b> – chavecar.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
