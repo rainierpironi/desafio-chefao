ff4e817833344c860801e84fb43ee2080047f0dc
unknown <eniofilho76@gmail.com>
2022-07-06 16:43:51 -0300

    pagina culturais concluida e responsiva finalmente !

--- frontend/src/components/Culturais/Culturaisfirst/Culturaisfirst.tsx
+++ frontend/src/components/Culturais/Culturaisfirst/Culturaisfirst.tsx
@@ -1,28 +1,20 @@
 import React, { Fragment } from 'react'
 import { Figure } from 'react-bootstrap'
 import "../../Culturais/Culturaisfirst/Culturaisfirst.css"
-import Imagem from "../../../assets/Images/culturaisdesk.png"
-import Faixa from "../../../assets/Images/faixacult.png"
+
 
 function Culturaisfirst() {
   return (
     <Fragment>
-      <main className='containercult'>
-        
-        <div className='containerBack'>
-          <img src={Imagem} alt="" />
-        </div>
-        
-        <div className='titulocult'>
+       <div className='containerPlanFirst'>
+      <div className='containerPlanTituloFirst'>
+        <div className='tituloSegFirst'>
           <h1>Dicas Culturais</h1>
-          <p>Conhecendo a riqueza das diferenças culturais no Brasil...</p>
+          <p>Conhecendo a riqueza das diferenças culturais no Brasi... 
+</p>
         </div>
-        <div className='faixacult'>
-          <img src={Faixa} alt="" />
+      </div>
         </div>
-
-      </main>
-
     </Fragment>
   )
 }
