ffd7730d3e93b70c2ea54e375af0f052891bd293
unknown <eniofilho76@gmail.com>
2022-07-06 17:42:10 -0300

    pagina uteis finalizada e responsiva aleleuia!

--- frontend/src/components/Uteis/Uteissecond/Uteissecond.tsx
+++ frontend/src/components/Uteis/Uteissecond/Uteissecond.tsx
@@ -1,268 +1,67 @@
 import React, { Fragment } from 'react'
 import { Accordion } from 'react-bootstrap'
 import "../Uteissecond/Uteissecond.css"
+import FaixaUtil from "../../../assets/Images/faixauteis.png"
 
 function Uteissecond() {
     return (
         <Fragment>
-
-            <div className='containerSecond'>
-
-                <div className='borderSecond'></div>
-                <div className='aereasSecond'>Delegacias da Mulher</div>
-                <div className='scrollSecond'>
-
+            <div className='containerUtilSecond'>
+                <div>
+                    <img id="faixa" src={FaixaUtil} alt="Faixa banner" />
+                </div>
+                <div className='tituloUtilSecond'>
+                    <p>Delegacias da Mulher</p>
+                </div>
+                <div className='dropUtilSecond'>
                     <Accordion >
-
-                        <div className='adjustSecond'>
+                        <div className='adjustUtilSecond'>
                             <Accordion.Item eventKey="0">
-                                <div className='sombra'>
-                                    <Accordion.Header>Delegacias da Mulher do Norte</Accordion.Header>
-                                </div>
+                                <Accordion.Header>Delegacias da Mulher do Norte</Accordion.Header>
                                 <Accordion.Body>
-                                    Acre
-                                    (68) 3221-0404 / 3221- 4799 / Deam.sepc@gmail.com
-                                    Via Chico Mendes, no 803, 2o Distrito – Bairro: Triângulo. Cep.: 69901-180 - Rio Branco/AC
-                                    (68) 3322-7440 / carla.ivane@gmail.com
-                                    Avenida 25 de Agosto, 1923 – Bairro: 25 de Agosto – Referência: Ao lado do Corpo de Bombeiros – Cruzeiro do Sul/AC Cep.: 69980- 000
-
-                                    Amazonas
-                                    (97) 3471-2757 / dip.sgc@outlook.com
-                                    Rua Quatro, no 103 – Bairro: Dabaru – Referência: funciona no mesmo endereço da Delegacia Interativa
-                                    (92)3236-7012 / deccmdelmulhermanaus@policiacivil.am.gov.br-
-                                    (97)3412-4072 / aeleandrogranja@policiacivil.am.gov.br
-                                    (92)3533-4325 / anadenapc@hotmail.com
-
-                                    Amapá
-                                    (96) 2101-2751
-                                    Rua Rio Juruá, s/no – Bairro: Centro – Referência: Próximo a Peixaria Amazonas Plaza Cep: 68900-000 Município: Macapá
-
-                                    Pará
-                                    (83) 3531-7022 / cristianapires@hotmail.com
-                                    Rua Romualdo Rolin, no 636 – Bairro: Centro. Cep.: 58900-000 - Cajazeiras/PB
-                                    (83)3228-6349 / mulher.cabedelo@ssp.pb.gov.br
-                                    Rua Pastor José Alves n° 357 – Bairro: Centro. Cep.: 58310-000 – Cabedelo/PB
-
-                                    Rondônia
-                                    (69)3536-8425 / ddm.ariquemes@pc.ro.gov.br
-                                    Travessa Cassiterita, no 1429 – Bairro: Setor Institucional – Referência: Atrás da Câmara Municipal - Ariquemes/RO. Cep.: 78932-000
-                                    (69)3441-5707 / ddm.cacoal@pc.ro.gov.br
-                                    Rua Inderval José Brasil, no 510 – Bairro: Novo Cacoal - Cacoal/RO. Cep.: 76962-219
-
-                                    Roraima
-                                    (95)991445614
-                                    R. Uraricoera, S/N, São Vicente, Boa Vista/RR – CEP 69303-453
-
-                                    Tocantins
-                                    (63)3464-2536 / drpc.guarai@hotmail.com
-                                    Rua Pernambuco Quadra 05 Lote 01 – Bairro: Setor Canaã Município: Guaraí/TO Cep: 77700-000
-                                    (63)3363-1682 / deamportonacional@ssp.to.gov.br
-                                    Avenida Rio de Janeiro, no 137 – Bairro: Santa Helena Município: Porto Nacional/TO Cep: 77500-000
-
-
-
+                                    <b>Acre</b> (68) 3221-0404 / 3221- 4799 / Deam.sepc@gmail.com <b>Amazonas</b> (97) 3471-2757 / dip.sgc@outlook.com
+                                   <b>Amapá</b> (96) 2101-2751 <b>Pará</b> (83) 3531-7022 / cristianapires@hotmail.com <b>Rondônia</b> (69)3536-8425 / ddm.ariquemes@pc.ro.gov.br <b>Roraima</b> (95)991445614 <b>Tocantins</b> (63)3464-2536 / drpc.guarai@hotmail.com
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
-                        <div className='adjustSecond'>
+                        <div className='adjustUtilSecond'>
                             <Accordion.Item eventKey="1">
-                                <div className='sombra'>
-                                    <Accordion.Header>Delegacias da Mulher do Nordeste</Accordion.Header>
-                                </div>
+                                <Accordion.Header>Delegacias da Mulher do Nordeste</Accordion.Header>
                                 <Accordion.Body>
-                                    Alagoas
-                                    (82) 3315-4976 / Deam.sepc@gmail.com
-                                    Rua Boa Vista, n. 443 – Bairro centro – Cep.: 57020-110 – Maceió/AL
-
-                                    Bahia
-                                    (73)3214-7822 / dip.sgc@outlook.com
-                                    Rua Rio Almada no 196 – Bairro: Góis Calmon, Cep: 45605-375 Município: Itabuna
-                                    (71)3622-7834 / deamcamacari@hotmail.com
-                                    Rua Delegado Clayton Leão Chaves, s/no – Bairro: Centro Cep: 42800-400 Município: Camaçari
-                                    (73)3268-8613 / deamportoseguroba@hotmail.com
-                                    Rua Itagibá, no 139 – Bairro: Centro, Cep: 45810-000 Município: Porto Seguro
-
-                                    Ceará
-                                    (96) 2101-2751
-                                    Rua Rio Juruá, s/no – Bairro: Centro – Referência: Próximo a Peixaria Amazonas Plaza Cep: 68900-000 Município: Macapá
-
-                                    Maranhão
-                                    (99)3621-1025 / maciagard@bol.com.br
-                                    Rua Eurico Gaspar Dutra, s/no – Bairro: Centro, Cep: 65700-000 Município: Bacabal
-                                    (99)3551-2402 / reg.saojoaodospatos12@hotmail.com
-
-                                    Paraíba
-                                    (83) 3531-7022 / cristianapires@hotmail.com
-                                    Rua Romualdo Rolin, no 636 – Bairro: Centro. Cep.: 58900-000 - Cajazeiras/PB
-
-                                    Pernambuco
-                                    (81)3524-1630 / 14delmulher@policiacivil.pe.gov.br
-                                    Rua Nova, no 233 – Bairro: Santo Inácio - Cabo de Santo Agostinho/PE. Cep.: 54510-520.
-                                    (81)3719-9106 / 4delmulher@policiacivil.pe.gov.br
-                                    Rua Dalton Santos, no 115 – Bairro: São Francisco - Caruaru/PE. Cep.: 55006-380
-
-                                    Piauí
-                                    (86)3222-2323 / deamcentro.te@pc.pi.gov.br
-                                    Rua 24 de Janeiro, no 500 – Bairro: Centro - Teresina/PI. Cep.: 64001-230
-                                    (86)3276-1111 / anamelka@pc.pi.gov.br
-                                    Rua João Damasceno s/no – Bairro: Centro - Piripiri/PI. Cep.: 64260- 000
-
-                                    Rio Grande do Norte
-                                    (84)3644-6425 / dulcineiacosta@rn.gov.br
-                                    Rua Sub Oficial Farias, no 1487 – Bairro: Centro – Parnamirim/RN. Cep.: 59140-255
-                                    (84)3232-2526 / deamvn@rn.gov.br
-                                    Rua Frei Miguelinho, no 109 – Bairro: Ribeira – Natal/RN. Cep.: 59012-180
-
-                                    Sergipe
-                                    (79)3431-8513 / dagv.itabaiana@pc.se.gov.br
-                                    Rua Vereador Olimpio Grande, no 177 – Bairro: Rotary Clube - Itabaiana/SE. Cep.: 49500-000.
-                                    (79)3631-7823 / dagv.lagarto@se.gov.br
-                                    Rua Limoeiro, s/no – Bairro: Centro – Referência: em frente à Adeso - Lagarto/SE. Cep.: 49400-000
-
-
-
+                                    <b>Alagoas</b> (82) 3315-4976 / Deam.sepc@gmail.com <b>Bahia</b> (73)3214-7822 / dip.sgc@outlook.com <b>Ceará</b> (96) 2101-2751 <b>Maranhão</b> (99)3621-1025 / maciagard@bol.com.br <b>Paraíba</b> (83) 3531-7022 / cristianapires@hotmail.com <b>Pernambuco</b> (81)3524-1630 / 14delmulher@policiacivil.pe.gov.br <b>Piauí</b> (86)3222-2323 / deamcentro.te@pc.pi.gov.br <b>Rio Grande do Norte</b> (84)3644-6425 / dulcineiacosta@rn.gov.br <b>Sergipe</b> (79)3431-8513 / dagv.itabaiana@pc.se.gov.br
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
-                        <div className='adjustSecond'>
+                        <div className='adjustUtilSecond'>
                             <Accordion.Item eventKey="2">
-                                <div className='sombra'>
-                                    <Accordion.Header>Delegacias da Mulher do Centro-Oeste</Accordion.Header>
-                                </div>
+                                <Accordion.Header>Delegacias da Mulher do Centro-Oeste</Accordion.Header>
                                 <Accordion.Body>
-                                Distrito Federal 
-(61)3207-8763 / dp38_saa@pcdf.df.gov.br
-Rua 04 A, Chácara 192 – Área Especial – Bairro: Vicente Pires Cep: 72110-800 Município: Brasília
-(61)3207-6535 / dp3_sam@pcdf.df.gov.br
-SREC, Área Especial, Lote 16 – Bairro: Cruzeiro Velho, Cep: 70640-000 Município: Brasília
-
-Goiás 
-(62)3201-2642 / deam-jatai@policiacivil.go.gov.br
-Rua Doutor Flavinho, Quadra 41 Lote 29, no 200 – Bairro: Setor Central, Cep: 75000-800 Município: Jataí
-(64)3454-6640 / deam-caldasnovas@policiacivil.go.gov.br
-Alameda Contorno s/no – Bairro: Centro, Cep: 75690-000 Município: Caldas Novas
-(64)3620-0950/ deam-rioverde@policiacivil.go.gov.br
-Rua 16, no 76 – Bairro: Popular, Cep: 75903-493 Município: Rio Verde
-
-Mato Grosso
-(65)3901-5326 / dmulhercda@policiacivil.gov.br
-Rua Coronel Peixoto, no 84 – Bairro: Bandeirantes Cep: 78010-100 Município: Cuiabá
-(65)3685-1236 / ddesmulhervg@pjc.mt.gov.br
-Avenida Dom Orlando Chaves, s/no – Bairro: Cristo Rei Cep: 78115-800 Município: Várzea Grande
-
-Mato Grosso do Sul
-(67) 3241-1172
-Rua Luiz da Costa Gomes, 555 – Alto Aquidauana Aquidauana – MS - CEP: 79.200-000
-(67) 3907-9900
-Rua Major Gama, 290 – Centro
-Corumbá – MS - CEP: 79.300-000
-(67) 3291-1338
-Rua General Mendes de Morais, 230 - Jardim Aeroporto Coxim – MS - CEP: 79.400-00
-
-
-
-.
+                                    <b>Distrito Federal</b> (61)3207-8763 / dp38_saa@pcdf.df.gov.br <b>Goiás</b> (62)3201-2642 / deam-jatai@policiacivil.go.gov.br <b>Mato Grosso</b>
+                                    (65)3901-5326 / dmulhercda@policiacivil.gov.br <b>Mato Grosso do Sul</b>(67) 3241-1172
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
-                        <div className='adjustSecond'>
+                        <div className='adjustUtilSecond'>
                             <Accordion.Item eventKey="3">
-                                <div className='sombra'>
-                                    <Accordion.Header>Delegacias da Mulher do Sudeste</Accordion.Header>
-                                </div>
+                                <Accordion.Header>Delegacias da Mulher do Sudeste</Accordion.Header>
                                 <Accordion.Body>
-                                Espírito Santo
-(27)3264-2537 / mara.costa@pc.es.gov.br
-Rua José Cândido Durão, esquina com a Rua Hélio José de Almeira, Lote 17/18/19, Quadra C3 – Bairro: Centro – Linhares/ES. Cep.: 29900-000
-(27) 3323-4045 / deam.vitoria@pc.es.gov.br
-Rua Hermes Curry Carneiro, no 350 – Bairro: Ilha de Santa Maria – Vitoria/ES. Cep.: 29051-210
-
-Minas Gerais
-(31)3769-1227 / mulheres.lafaiete@pc.mg.gov.br
-Praça Pimentel Duarte, s/no – Bairro: Centro, Cep: 36400-000 Município: Conselheiro Lafaiete
-(34)3312-9161 / elisabetecardoso@uberabadigital.com.br
-Rua Amaro Ferreira, no 221 – Bairro: Fabrício, Cep: 38010-140 Município: Uberaba
-(37)3241-3106 / delmulheres.itauna@pc.mg.gov.br
-Praça Dona Macrina, s/no – Bairro: Das Graças – Referência: Ao lado da Associação dos Contabilistas
-Cep: 35680-339 Município: Itaúna
-
-São Paulo
-(11)4138-3409 / tserra.ddm@policiacivil.sp.gov.br
-Estrada das Olarias, no 670 – Bairro: Jardim Guaciara Município: Taboão da Serra/SP. Cep: 06783-000
-(11)4514-1333 / ddmmaua@ig.com.br
-Rua General Osório, no 365 – Bairro: Vila Bocaína, Município: Mauá/SP Cep: 09310-050
-(11)4029-2533 / ddm.salto@gmail.com
-Rua Marechal Deodoro, no 291 – Bairro: Centro, Município: Salto/SP Cep: 13320-140
-(12)3621-5499 / taubate.ddm@policia-civ.sp.gov.br
-Alameda Investigador José Leôncio Ramos, s/no – Bairro: Jardim Eulália, Município: Taubaté/SP Cep: 12010-630
-
-Rio de Janeiro
-(21)3771-1135 / mariaaparecidamallete@pcivil.rj.gov
-Alameda Retiro da Imprensa, no 800 – Bairro: Piam - Belford Roxo/RJ. Cep.: 26112-18
-(21)3119-0214 / conorato@pcivil.rj.gov.br
-Avenida 18 do Forte, 578 – Bairro: Mutua - São Gonçalo/RJ. Cep.: 24460-005
-(21)2717-0558 / deam76atend@pcivil.rj.gov.br
-Avenida Ernani do Amaral Peixoto, no 577 – Bairro: Centro – Niterói/RJ. Cep.: 22735-130
-(21)2771-3434 / marcianoeli@pcivil.rj.gov.br
-Rua Brigadeiro Lima e Silva, no 1204 – Bairro: Duque de Caxias - Duque de Caxias/RJ. Cep.: 25071-180
-
-
-
-
-
-
+                                    <b>Espírito Santo</b> (27)3264-2537 / mara.costa@pc.es.gov.br Minas <b>Gerais</b> (31)3769-1227 / mulheres.lafaiete@pc.mg.gov.br <b>São Paulo</b>
+                                    (11)4138-3409 / tserra.ddm@policiacivil.sp.gov.br <b>Rio de Janeiro</b> (21)3771-1135 / mariaaparecidamallete@pcivil.rj.gov
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
-                        <div className='adjustSecond'>
+                        <div className='adjustUtilSecond'>
                             <Accordion.Item eventKey="4">
-                                <div className='sombra'>
-                                    <Accordion.Header>Delegacias da Mulher do Sul</Accordion.Header>
-                                </div>
+                                <Accordion.Header>Delegacias da Mulher do Sul</Accordion.Header>
                                 <Accordion.Body>
-                                Paraná
-(41)3698-9072 / dpalmirantetamandare@pc.pr.gov.br
-Rua Coronel João Cândido de Oliveira, no 333 – Bairro: Centro - Almirante Tamandaré/PR. Cep.: 83501-010
-(41)3292-1203 / dpcampolargo@pc.pr.gov.br
-Rua Joanim Stroparo, no 786 – Bairro: Centro - Campo Largo/PR. Cep.: 83601-460
-(41)3219-8600 / dpmulhercapital@pc.pr.gov.br
-Rua Padre Antônio, no 33 – Bairro: Centro – Referência: Ao lado do Colégio Estadual - Curitiba/PR. Cep.: 80030-100
-
-Santa Catarina
-(47)3533-1468 / dpcamiituporanga@pc.sc.gov.br
-Rua Joaquim Boeing, no 123 – Bairro: Centro Município: Ituporanga/SC Cep: 88400-000
-(47)3354-0661 / dpbrusque@pc.sc.gov.br
-Rua do Convento, no 49 – Bairro: Centro Município: Brusque/SC Cep: 88350-380
-(47)3631-7751/ dpcamisaobentodosul@pc.sc.gov.br
-Rua Jorge Zipperer, no 544 – Bairro: Centro Município: São Bento do Sul/SC Cep: 89280-499
-
-Rio Grande do Sul
-(53)3225-6888 / pelotas-dm@pc.rs.gov.br
-Rua Barros Cassal, no 516, Térreo – Bairro: Areal – Referência: Ao lado do Fórum - Pelotas/RS. Cep.: 96077-540
-(51)3288-2172 / poa-dm@pc.rs.gov.br
-Avenida João Pessoa, no 2050 – Bairro: Azenha – Referência: No Palácio da Polícia - Porto Alegre/RS. Cep.: 90040-001
-(51)3711-4513 / santacruz-dm@pc.rs.gov.br
-Rua João Werlang, no 569 – Bairro: Belvedere - Santa Cruz do Sul/RS. Cep.: 96825-473
-
-
-
-
-
-
+                                    <b>Paraná</b> (41)3698-9072 / dpalmirantetamandare@pc.pr.gov.br <b>Santa Catarina</b> (47)3533-1468 / dpcamiituporanga@pc.sc.gov.br
+                                    <b>Rio Grande do Sul</b> (53)3225-6888 / pelotas-dm@pc.rs.gov.br
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
-
                     </Accordion>
                 </div>
-            </div >
-
-
+            </div>
         </Fragment >
     )
 }
--- frontend/src/components/Uteis/Uteisthird/Uteisthird.tsx
+++ frontend/src/components/Uteis/Uteisthird/Uteisthird.tsx
@@ -15,10 +15,7 @@ function Uteisthird() {
                                     <Accordion.Header>Delegacias do Turista no Norte</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Amazonas
-                                    Delegacia Especializada em Crimes contra o Turista - DECCT
-                                    Mezanino do Porto de Manaus e Aeroporto – Manaus/AM
-                                    Telefone: (92) 3652-1656/ 3233-0739
+                                    <b>Amazonas</b> Delegacia Especializada em Crimes contra o Turista - DECCT – Mezanino do Porto de Manaus e Aeroporto – Manaus/AM  (92) 3652-1656/ 3233-0739
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
@@ -30,35 +27,30 @@ function Uteisthird() {
                                     <Accordion.Header>Delegacias do Turista no Nordeste</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                    Bahia
+                                    <b>Bahia</b>
                                     Delegacia de Proteção ao Turista - DELTUR
                                     Cruzeiro de São Francisco – Pç. Anchieta nº 14.
                                     Bairro: Centro Histórico – Salvador/BA
                                     Telefone: (71) 3116-6817
-
-                                    Ceará
+                                    <b> Ceará</b>
                                     Delegacia de Apoio ao Turista – DEPROTUR
                                     Rua Costa Barro Nº1971 – Fortaleza/CE
                                     Telefone: (85) 3101-2488
-
-                                    Paraíba
+                                    <b>Paraíba</b>
                                     Delegacia de Apoio ao Turista
                                     Av. Almirante Tamandaré, nº100 – João Pessoa/PB
                                     Telefone: (83) 3214-8022/ 32478405
-
-                                    Pernambuco
+                                    <b>Pernambuco</b>
                                     Delegacia de Polícia do Turista – DPTUR
                                     Aeroporto Internacional dos Guararapes – Recife/PE
                                     Telefone: (81) 3184-3437/ 3322-4088/ 3322-4867/ 3184-3438
-
-                                    Rio Grande do Norte
+                                    <b>Rio Grande do Norte</b>
                                     Delegacia Especializada em Assistência ao Turista e Meio
                                     Ambiente – DEATUR/ DEPREMA
                                     Praia Shopping – Ponta Negra
                                     Avenida Engenheiro Roberto Freire, 8790 – Ponta Negra – Natal/RN
                                     Telefone: (84) 3232-7402
-
-                                    Sergipe
+                                    <b>Sergipe</b>
                                     Delegacia Especial de Turismo
                                     Praça Santos Dumont, Box 1710 – Atalaia – Aracaju/SE
                                     Telefone: (79) 3198-1118
@@ -74,67 +66,30 @@ function Uteisthird() {
                                     <Accordion.Header>Delegacias do Turista no Centro-Oeste</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                Mato Grosso do Sul
-Mato Grosso do Sul 
-Delegacia Especializada de Repressão a Crimes Ambientais e Proteção ao Turista – DECAT
-Av. Duque de Caxias, s/n°
-Aeroporto Internacional (Mezanino) – Campo Grande/MS
-Telefone: (67) 3368-6144
-
-
-
-
-
-
+                                    <b>Mato Grosso do Sul</b> Delegacia Especializada de Repressão a Crimes Ambientais e Proteção ao Turista, DECAT (67) 3368-6144
+                                    Aeroporto Internacional (Mezanino) – Campo Grande/MS
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
                         <div className='adjustPlan'>
                             <Accordion.Item eventKey="3">
                                 <div className='sombra'>
                                     <Accordion.Header>Delegacias do Turista no Sudeste</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                Espírito Santo
-Delegacia de Apoio ao Turista – DPTUR
-Avenida Américo Buaiz, 200, Enseada do Suá (Shopping Vitória) – Vitória/ES
-Telefone: (27) 3137-9117
-
-São Paulo
-Delegacia Especializada no Atendimento ao Turista – DEATUR
-Rua Cantareira Nº390, Centro – São Paulo/SP
-Telefone: (11) 3257-4475
-
-Rio de Janeiro
-Delegacia Especial de Apoio ao Turismo – DEAT
-Avenida Afrânio de Melo Franco, 159 – Leblon – Rio de Janeiro/RJ
-Telefone: (21) 2332-2924/ 2332-2885/ 2332-2889
-
-
-
-
-
-
+                                    <b>Espírito Santo</b> Delegacia de Apoio ao Turista, DPTUR (27) 3137-9117, Avenida Américo Buaiz, 200, Enseada do Suá, Vitória/ES
+                                    <b>São Paulo</b> Delegacia Especializada no Atendimento ao Turista, DEATUR  (11) 3257-4475, Rua Cantareira Nº390, Centro – São Paulo/SP <b>Rio de Janeiro</b> Delegacia Especial de Apoio ao Turismo, DEAT (21) 2332-2924/ 2332-2885/ 2332-2889, Avenida Afrânio de Melo Franco, 159 – Leblon – Rio de Janeiro/RJ
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
-
                         <div className='adjustPlan'>
                             <Accordion.Item eventKey="4">
                                 <div className='sombra'>
                                     <Accordion.Header>Delegacias do Turista no Sul</Accordion.Header>
                                 </div>
                                 <Accordion.Body>
-                                Santa Catarina
-Delegacia de Proteção ao Turista – DPTUR
-Avenida Paulo Fontes,1101 Centro – Florianópolis/SC
-Telefone: (48) 3665-5723
+                                <b>Santa Catarina</b> Delegacia de Proteção ao Turista, DPTUR (48) 3665-5723, Avenida Paulo Fontes,1101 Centro – Florianópolis/SC <b>Rio grande do Sul</b>Delegacia de Apoio ao Turista, DPTUR (51) 3358-2255, Setor de Desembarque do Aeroporto Salgado Filho – Porto Alegre/RS
 
-Rio grande do Sul
-Delegacia de Apoio ao Turista – DPTUR
-Setor de Desembarque do Aeroporto Salgado Filho – Porto Alegre/RS
-Telefone: (51) 3358-2255
                                 </Accordion.Body>
                             </Accordion.Item>
                         </div>
