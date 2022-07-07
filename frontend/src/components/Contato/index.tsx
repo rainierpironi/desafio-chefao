b8fa5a5c1afbe3d38791d06d71a593dda8fcd608
Roberto Chagas <55409496+RobertoChagas@users.noreply.github.com>
2022-07-07 11:43:38 -0300

    ajustes na responsividade mobile do form de contato na home

--- frontend/src/components/Contato/index.tsx
+++ frontend/src/components/Contato/index.tsx
@@ -41,54 +41,65 @@ const FormContato: React.FC = () => {
                 <div className='contato'>
                     <h1>Contato</h1>
                     <h4>A sua opinião é importante</h4>
-                <p>Buscamos entender as necessidades das mulheres viajantes solo para oferecer funcionalidades que agreguem valor nas suas experiências como usuárias dessa plataforma. 
-                </p>
-                <p>
-                Gostaria de fazer algum comentário, sugestão ou reclamação? 
-                </p>
-                <p>
-                    Conta pra gente!
-                </p>
-                <p>
-                Para entrar em contato conosco, preencha os campos ao lado, e iremos entrar em contato com você o mais breve possível.
-                </p>
+                    <p>Buscamos entender as necessidades das mulheres viajantes solo para oferecer funcionalidades que agreguem valor nas suas experiências como usuárias dessa plataforma.
+                    </p>
+                    <p>
+                        Gostaria de fazer algum comentário, sugestão ou reclamação?
+                    </p>
+                    <p>
+                        Conta pra gente!
+                    </p>
+                    <p>
+                        Para entrar em contato conosco, preencha os campos ao lado, e iremos entrar em contato com você o mais breve possível.
+                    </p>
                 </div>
                 <div className='contato-form'>
                     <form onSubmit={formik.handleSubmit}>
                         <div className='nome-email'>
-                            <Form.Group className="mb-3" controlId="nome">
-                                <Form.Label className='titulo-input'>Nome</Form.Label>
-                                <Form.Control type="text"
-                                    placeholder="Digite aqui o seu nome..."
-                                    style={{ height: '50px', 
-                                     width:'45vh', 
-                                     background: '#2a2b2c',
-                                     borderRadius:'30px'  }}
-                                    defaultValue={formik.values.nome}
-                                    onChange={formik.handleChange} />
-                                {formik.errors.nome && <span>{formik.errors.nome}</span>}
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="email">
-                                <Form.Label className='titulo-input'>E-mail</Form.Label>
-                                <Form.Control type="email"
-                                    placeholder="Digite aqui o seu e-mail..."
-                                    style={{ height: '50px', 
-                                    width:'45vh', 
-                                    background: '#2a2b2c',
-                                    borderRadius:'30px'  }}
-                                    defaultValue={formik.values.email}
-                                    onChange={formik.handleChange} />
-                                {formik.errors.email && <span>{formik.errors.email}</span>}
-                            </Form.Group>
+                            <div>
+                                <Form.Group className="mb-3 nome" controlId="nome">
+                                    <Form.Label className='titulo-input'>Nome</Form.Label>
+                                    <Form.Control type="text"
+                                        placeholder="Digite aqui o seu nome..."
+                                        style={{
+                                            height: '50px',
+                                            width: '20rem',
+                                            background: '#2a2b2c',
+                                            borderRadius: '30px'
+                                        }}
+                                        defaultValue={formik.values.nome}
+                                        onChange={formik.handleChange} />
+                                    {formik.errors.nome && <span>{formik.errors.nome}</span>}
+                                </Form.Group>
+                            </div>
+                            <div>
+                                <Form.Group className="mb-3 " controlId="email">
+                                    <Form.Label className='titulo-input '>E-mail</Form.Label>
+                                    <Form.Control type="email"
+                                        placeholder="Digite aqui o seu e-mail..."
+                                        style={{
+                                            height: '50px',
+                                            width: '20rem',
+                                            background: '#2a2b2c',
+                                            borderRadius: '30px'
+                                        }}
+
+                                        defaultValue={formik.values.email}
+                                        onChange={formik.handleChange} />
+                                    {formik.errors.email && <span>{formik.errors.email}</span>}
+                                </Form.Group>
+                            </div>
                         </div>
                         <div>
                             <Form.Group className="mb-3" controlId="assunto">
                                 <Form.Label className='titulo-input'>Assunto</Form.Label>
                                 <Form.Control type="text"
                                     placeholder="Digite aqui o assunto da mensagem..."
-                                    style={{ height: '50px', 
-                                             background: '#2a2b2c',
-                                             borderRadius:'30px'  }}
+                                    style={{
+                                        height: '50px',
+                                        background: '#2a2b2c',
+                                        borderRadius: '30px'
+                                    }}
                                     defaultValue={formik.values.assunto}
                                     onChange={formik.handleChange} />
                                 {formik.errors.assunto && <span>{formik.errors.assunto}</span>}
@@ -99,24 +110,28 @@ const FormContato: React.FC = () => {
                                 <Form.Label className='titulo-input'>Mensagem</Form.Label>
                                 <Form.Control as="textarea" rows={10}
                                     placeholder="Digite aqui sua mensagem até 280 caracteres..."
-                                    style={{ background: '#2a2b2c', 
-                                             borderRadius:'30px' }}
+                                    style={{
+                                        background: '#2a2b2c',
+                                        borderRadius: '30px'
+                                    }}
                                     defaultValue={formik.values.mensagem}
                                     onChange={formik.handleChange} />
                                 {formik.errors.mensagem && <span>{formik.errors.mensagem}</span>}
                             </Form.Group>
                         </div>
                         <div className='form-btn'>
-                            <Button style={{ width: '128.08px', 
-                                             height: '7vh',
-                                             borderRadius:'30px',
-                                             boxShadow:' 1px 5px 5px black',
-                                             fontStyle:'bold',
-                                             fontSize:'20px',
-                                             display:'flex',
-                                             alignItems:'center',
-                                             justifyContent:'center' }} 
-                                     variant="warning" type="submit">
+                            <Button style={{
+                                width: '8rem',
+                                height: '3rem',
+                                borderRadius: '30px',
+                                boxShadow: ' 1px 5px 5px black',
+                                fontStyle: 'bold',
+                                fontSize: '20px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                                variant="warning" type="submit">
                                 Enviar
                             </Button>
                         </div>
