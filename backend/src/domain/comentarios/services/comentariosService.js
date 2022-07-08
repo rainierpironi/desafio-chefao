23e8876817f9bab9f956415e22214d56b910ec4a
TikYokoyama <tiaki.yokoyama97@gmail.com>
2022-07-08 14:59:35 -0300

    adicionando comentarioServices

---
+++ backend/src/domain/comentarios/services/comentariosService.js
@@ -0,0 +1,70 @@
+const Comentarios = require('../models/Comentarios');
+
+const ComentarioServices = {
+
+    async criaComent(data) {
+        const { nome, email, mensagem, destinos_id } = data
+
+        const novoComent = await Comentarios.create({
+            nome,
+            email,
+            mensagem,
+            destinos_id
+        })
+
+        return novoComent;
+    },
+
+    async encontraComent(id) {
+        const checaComent = await Comentarios.findOne({where:{id:id}});
+        return checaComent;
+      },
+
+    async listaComents() {
+        const listaComents = await Comentarios.findAll();
+        return listaComents
+    },
+
+    async listaComentIdDestino(req) {
+        const { id } = req.params;
+
+        const listComentDestino = await Comentarios.findByPk(id);
+        return listComentDestino
+    },
+
+    async atualizaComent(req) {
+        const { id } = req.params;
+        const { nome, email, mensagem, destinos_id } = req.body;
+
+        const atualizarComent = await Comentarios.update(
+            {
+                nome,
+                email,
+                mensagem,
+                destinos_id,
+            },
+            {
+                where: {
+                    id,
+                }
+            }
+        );
+        const ComentUpdated = await Comentarios.findByPk(id);
+        return ComentUpdated;
+    },
+
+    async deletaComent(id){
+        const deletaComent = await Comentarios.destroy({
+            where:{
+                id
+            }
+        })
+
+        return deletaComent
+    }
+
+
+}
+
+
+module.exports = ComentarioServices;
