8a8d85a4952c52b7f01f47146a4e49166d703a19
Roberto Chagas <55409496+RobertoChagas@users.noreply.github.com>
2022-07-08 18:51:54 -0300

    ajuste na altura do componente, bem como correção das linhas que separam as conteúdos

--- frontend/src/components/Cidades/FotosCidades/index.tsx
+++ frontend/src/components/Cidades/FotosCidades/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Container from '../../Container';
+import React, {Fragment} from 'react';
 import linhaPontilhada from "../../../assets/Images/Olinda/dashed-border.png";
 import igrejaSe from "../../../assets/Images/Olinda/igreja-se.svg";
 import miranteSe from "../../../assets/Images/Olinda/mirante-se.svg";
@@ -11,8 +10,11 @@ import './fotos-cidades.css';
 
 const FotosCidades: React.FC = () => {
   return (
+    <Fragment>
+      <div className='borda-dashed'>
+        <img id='borda-dashed' src={linhaPontilhada} alt="linha amarela pontilhada" />
+      </div>
       <div className='main-fotos-cidades'>
-        <img id='borda-dashed' src={linhaPontilhada} alt="" />
         <h2>Lugares interessantes para conhecer</h2>
         <div className='container-fotos-cidades'>
           <div>
@@ -33,6 +35,8 @@ const FotosCidades: React.FC = () => {
           </div>
         </div>
       </div>
+    </Fragment>
+
   );
 }
 
