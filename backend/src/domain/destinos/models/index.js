b91ba3d4338416a582e5016a7e00fae9a20d2009
Ivan Yura Takano <ivanyurat@gmail.com>
2022-07-08 10:07:55 -0300

    excluindo requisições  de filtro de destinos  do controller e models de destinos

--- backend/src/domain/destinos/models/index.js
+++ backend/src/domain/destinos/models/index.js
@@ -1,10 +1,6 @@
 const Destinos = require("./Destinos"),  
   Comentarios = require("../../comentarios/models/Comentarios");
 
-Destinos.hasMany(Destinos_filtros, {
-  foreignKey: "id"
-});
-
 Destinos.hasMany(Comentarios, {
   foreignKey: "id"
 });
