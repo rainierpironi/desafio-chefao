6d4dbaa0174f5fb32318089e9ce67f709ef4c740
unknown <eniofilho76@gmail.com>
2022-07-09 15:53:56 -0300

    textos ajustados

--- frontend/src/components/Culturais/Culturaissecond/Culturaissecond.tsx
+++ frontend/src/components/Culturais/Culturaissecond/Culturaissecond.tsx
@@ -34,7 +34,7 @@ function Culturaissecond() {
                     <Accordion.Item eventKey="2">
                         <Accordion.Header>Expressões do Centro-Oeste</Accordion.Header>
                         <Accordion.Body>
-                        <b>ABISCOITAR</b> – receber dinheiro ou herança. <b>ARRUINOU</b> – piorou de saúde. <b>BERERÉ</b> – propina, dinheiro ganhado fácil. <b>BITELO</b> – algo grande. <b>BORRACHO</b> – bêbado. <b>CAMELA</b> – bicicleta. <b>CARRETA</b> – carro. <b>COMER NA GAVETA</b> – se relaciona a uma pessoa que é avarenta, um pão-duro. <b>DESCABRIADO</b> – descontrolado. <b>DORMIR NO MACIO</b> – viver folgado. <b>EMPATAR</b> – atrapalhar. <b>MADURAR</b> – amadurecer. <b>NÃO DÁ CONTA</b> – não consegue fazer algo que precisa ser feito. <b>PARA DE MULA</b> – perturbar. <b>PATIFE</b> – alguém medroso. <b>PÉ-DE-BOI</b> – pessoa que é trabalhadora. <b>PERRENGUE</b> – pessoa está adoentada ou enfraquecida. <b>TÁ CHOVENDO DURO</b> – chover torrencialmente.
+                        <b>ABISCOITAR </b> – receber dinheiro ou herança. <b>ARRUINOU</b> – piorou de saúde. <b>BERERÉ</b> – propina, dinheiro ganhado fácil. <b>BITELO</b> – algo grande. <b>BORRACHO</b> – bêbado. <b>CAMELA</b> – bicicleta. <b>CARRETA</b> – carro. <b>COMER NA GAVETA</b> – se relaciona a uma pessoa que é avarenta, um pão-duro. <b>DESCABRIADO</b> – descontrolado. <b>DORMIR NO MACIO</b> – viver folgado. <b>EMPATAR</b> – atrapalhar. <b>MADURAR</b> – amadurecer. <b>NÃO DÁ CONTA</b> – não consegue fazer algo que precisa ser feito. <b>PARA DE MULA</b> – perturbar. <b>PATIFE</b> – alguém medroso. <b>PÉ-DE-BOI</b> – pessoa que é trabalhadora. <b>PERRENGUE</b> – pessoa está adoentada ou enfraquecida. <b>TÁ CHOVENDO DURO</b> – chover torrencialmente.
                         </Accordion.Body>
                     </Accordion.Item>
                     </div>
@@ -42,7 +42,7 @@ function Culturaissecond() {
                     <Accordion.Item eventKey="3">
                         <Accordion.Header>Expressões do Sudeste</Accordion.Header>
                         <Accordion.Body>
-                        <b>BATER UM RANGO</b> – comer, matar a fome. <b>BOLADO</b> – preocupado. <b>CHAPAR O COCO</b> – beber muito. <b>DA HORA</b> – legal. <b>É FRIA</b> – é perigoso. <b>JACÚ</b> – bobo, ignorante. <b>LARICA</b> – fome. <b>MAROMBADO</b> – pessoa que faz musculação, quem é musculoso. <b>PADOCA</b> – padaria. <b>QUEBRADO</b> – sem dinheiro. <b>ROLÊ</b> – sair, dar uma volta, passeio. <b>SANGUE BOM</b> – pessoa legal, gente fina. <b>SÓ O PÓ</b> – cansado. <b>TREM</b> – qualquer coisa, qualquer objeto. <b>TRETA</b> – confusão, briga. <b>TROCAR IDEIA</b> – conversar. <b>ZUEIRA</b> – brincadeira, bagunça.
+                        <b>BATER UM RANGO </b> – comer, matar a fome. <b>BOLADO </b> – preocupado. <b>CHAPAR O COCO </b> – beber muito. <b>DA HORA </b> – legal. <b>É FRIA </b> – é perigoso. <b>JACÚ </b> – bobo, ignorante. <b>LARICA</b> – fome. <b>MAROMBADO </b> – pessoa que faz musculação, quem é musculoso. <b>PADOCA </b> – padaria. <b>QUEBRADO</b> – sem dinheiro. <b>ROLÊ</b> – sair, dar uma volta, passeio. <b>SANGUE BOM</b> – pessoa legal, gente fina. <b>SÓ O PÓ </b> – cansado. <b>TREM </b> – qualquer coisa, qualquer objeto. <b>TRETA </b> – confusão, briga. <b>TROCAR IDEIA</b> – conversar. <b>ZUEIRA </b> – brincadeira, bagunça.
                         </Accordion.Body>
                     </Accordion.Item>
                     </div>
@@ -50,26 +50,14 @@ function Culturaissecond() {
                     <Accordion.Item eventKey="4">
                         <Accordion.Header>Expressões do Sul</Accordion.Header>
                         <Accordion.Body>
-                        <b>ALÇAR A PENA</b> – montar no cavalo. <b>BAH</b> – é uma expressão usada em diversas situações e no início das frases. <b>CACETINHO</b> – pão francês. <b>CAIR OS BUTIÁ DO BOLSO</b> – se assustar, ficar surpreso com algo. <b>DAR UMA BANDA</b> – sair, dar uma volta. <b>DEITAR O CABELO</b> – fugir, cair fora. <b>DESABOTINADO</b> – adoidado, estourado. <b>EMPRETAR-SE</b> – meter-se em apuros. <b>ESGUALEPADO</b> – ferido, machucado. <b>GUAIPECA</b> – cachorro, vira-lata. <b>GURI E GURIA</b> – menino e menina. <b>LAGARTEAR</b> – ficar no sol. <b>LINDEIRO</b> – vizinho. <b>SOLITO</b> – sozinho, sem companhia. <b>TCHÊ</b> – usado geralmente no fim das frases para chamar atenção. <b>TRI</b> – bom, legal, muito maneiro. TROVAR – chavecar.
+                        <b>ALÇAR A PENA </b> – montar no cavalo. <b>BAH </b> – é uma expressão usada em diversas situações e no início das frases. <b>CACETINHO </b> – pão francês. <b> CAIR OS BUTIÁ DO BOLSO</b> – se assustar, ficar surpreso com algo. <b>DAR UMA BANDA </b> – sair, dar uma volta. <b>DEITAR O CABELO </b> – fugir, cair fora. <b>DESABOTINADO </b> – adoidado, estourado. <b>EMPRETAR-SE</b> – meter-se em apuros. <b>ESGUALEPADO </b> – ferido, machucado. <b>GUAIPECA </b> – cachorro, vira-lata. <b>GURI E GURIA </b> – menino e menina. <b>LAGARTEAR</b> – ficar no sol. <b>LINDEIRO </b> – vizinho. <b>SOLITO </b> – sozinho, sem companhia. <b>TCHÊ </b> – usado geralmente no fim das frases para chamar atenção. <b>TRI </b> – bom, legal, muito maneiro. TROVAR – chavecar.
                         </Accordion.Body>
                     </Accordion.Item>
                     </div>
-
-                    
-
-                    
-
-
-
                 </Accordion>
             </div>
-
-
-
         </div>
         </Fragment >
     )
-
 }
-
 export default Culturaissecond
--- frontend/src/components/Seguranca/Segurancasecond/index.tsx
+++ frontend/src/components/Seguranca/Segurancasecond/index.tsx
@@ -3,7 +3,7 @@ import "./styles.css"
 import FaixaSeg from "../../../assets/Images/segfaixa.png"
 import { Accordion } from 'react-bootstrap';
 
-// import { Container } from './styles';
+
 
 const SegurancaSecond: React.FC = () => {
     return (
@@ -19,19 +19,14 @@ const SegurancaSecond: React.FC = () => {
                     <Accordion.Item eventKey="0">
                         <Accordion.Header>Cuidados essenciais</Accordion.Header>
                         <Accordion.Body>
-                           <b> Evite desembarcar tarde da noite</b> atentando ao horário de desembarque no destino escolhido ao comprar sua passagem.
-                            <b>Fique atenta ao pegar táxi ou transporte público</b> já que não é raro um taxista ou motorista de aplicativo fazer um caminho mais longo ao notar que o cliente não conhece a cidade. Tenha um mapa e confira o percurso antes de entrar no veículo.
-                            <b>Escolha bem a acomodação</b> atentando sobre a localização e às avaliações. <b>Deixe uma cópia do seu roteiro com alguém da família ou amigos</b> além de manter contato diariamente, caso aconteça algo, as pessoas poderão ajudar, ou dar informações sobre a sua viagem ou chamando a emergência. <b>Evite caminhar à noite sozinha</b> por mais que pegar um táxi ou o transporte público saia mais caro, é melhor do que perder seus documentos e pertences em um lugar desconhecido. <b>Procure passeios em grupo</b> você pode fazer algumas amizades. <b>Não exagere no álcool</b> ele pode alterar a sua visão e a sua percepção tornando-a mais suscetível a roubo. <b>Leve seus mecanismos de defesa portátil para todo lugar</b> contra roubos ou abusadores. Spray de pimenta ou algo que possa te ajudar a se defender, como um chaveiro pontudo por exemplo. <b>Não saia sem seu telefone</b> ele é um item fundamental. Leve seu carregador na bolsa, confira o nível de bateria ao sair e tenha sempre na bolsa o endereço e o telefone do seu hotel. <b>Tenha todos os documentos essenciais para viajar sozinha</b> – RG, CNH ou outro documento de identificação pessoal oficial para viagem em território nacional, além do comprovante de vacinação contra o COVID-19.<b> Cuide da sua alimentação</b> procurando conhecer detalhes da gastronomia do seu destino, assim, evitando dores de cabeça futuras. O seguro viagem, mesmo em viagem nacional, é imprescindível para que se algo acontecer com você ou com sua bagagem, o atendimento esteja garantido.
+                           <b>EVITE DESEMBARCAR TARDE DA NOITE </b> atentando ao horário de desembarque no destino escolhido ao comprar sua passagem.
+                            <b>FIQUE ATENTA AO PEGAR TÁXI OU TRANSPORTE PÚBLICO </b> já que não é raro um taxista ou motorista de aplicativo fazer um caminho mais longo ao notar que o cliente não conhece a cidade. Tenha um mapa e confira o percurso antes de entrar no veículo.
+                            <b>ESCOLHA BEM A ACOMODAÇÃO </b> atentando sobre a localização e às avaliações. <b>DEIXE UMA CÓPIA DO SEU ROTEIRO COM ALGUÉM DA FAMÍLIA OU AMIGOS </b> além de manter contato diariamente, caso aconteça algo, as pessoas poderão ajudar, ou dar informações sobre a sua viagem ou chamando a emergência. <b>EVITE CAMINHAR À NOITE SOZINHA </b> por mais que pegar um táxi ou o transporte público saia mais caro, é melhor do que perder seus documentos e pertences em um lugar desconhecido. <b>PROCURE PASSEIOS EM GRUPO </b> você pode fazer algumas amizades. <b>NÃO EXAGERE NO ÁLCOOL </b> ele pode alterar a sua visão e a sua percepção tornando-a mais suscetível a roubo. <b>LEVE SEUS MECANISMOS DE DEFESA PORTÁTIL PARA TODO LUGAR </b> contra roubos ou abusadores. Spray de pimenta ou algo que possa te ajudar a se defender, como um chaveiro pontudo por exemplo. <b>NÃO SAIA SEM SEU TELEFONE </b> ele é um item fundamental. Leve seu carregador na bolsa, confira o nível de bateria ao sair e tenha sempre na bolsa o endereço e o telefone do seu hotel. <b>TENHA TODOS OS DOCUMENTOS ESSENCIAIS PARA VIAJAR SOZINHA</b> – RG, CNH ou outro documento de identificação pessoal oficial para viagem em território nacional, além do comprovante de vacinação contra o COVID-19.<b> CUIDE DA SUA ALIMENTAÇÃO </b> procurando conhecer detalhes da gastronomia do seu destino, assim, evitando dores de cabeça futuras. O seguro viagem, mesmo em viagem nacional, é imprescindível para que se algo acontecer com você ou com sua bagagem, o atendimento esteja garantido.
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
             </div>
-
-
-
         </div>
-
     )
 }
-
 export default SegurancaSecond;
