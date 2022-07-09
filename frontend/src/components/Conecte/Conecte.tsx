42c092081495882aad4e22bf9e35eb14d6df3a64
unknown <eniofilho76@gmail.com>
2022-07-09 16:34:45 -0300

    link telegram

--- frontend/src/components/Conecte/Conecte.tsx
+++ frontend/src/components/Conecte/Conecte.tsx
@@ -24,27 +24,27 @@ function Conecte() {
         <div ></div>
         <div className="containerimg">
           <div className='linkRegiao'>
-            <Link to="/destinos/norte">
+            <Link to="https://t.me/+UxRyV5jcw89jMTFh">
               <img id="efeito" src={Norte} alt="Mapa norte" />
             </Link>
           </div>
           <div className='linkRegiao'>
-            <Link to="/destinos/nordeste">
+            <Link to="https://t.me/+J1SsMApRiao2NDE5">
               <img id="efeito" src={Nordeste} alt="Ma´´a Nordeste" />
             </Link>
           </div>
           <div className='linkRegiao'>
-            <Link to="/destinos/centro-oeste">
+            <Link to="https://t.me/+Ktv_mYFjP6U5NzQx">
               <img id="efeito" src={CentroOeste} alt="Mapa centro oeste" />
             </Link>
           </div>
           <div className='linkRegiao'>
-            <Link to="/destinos/sudeste">
+            <Link to="https://t.me/+mZB7O0UQRSI2NTI5">
               <img id="efeito" src={Sudeste} alt="Mapa Sudeste" />
             </Link>
           </div>
           <div className='linkRegiao'>
-            <Link to="/destinos/sul">
+            <Link to="https://t.me/+LFjNJdDXj2o1MmU5">
               <img id="efeito" src={Sul} alt=" Mapa Sul" />
             </Link>
           </div>
