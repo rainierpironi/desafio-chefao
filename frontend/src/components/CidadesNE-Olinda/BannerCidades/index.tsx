da07794fb1e939974cc9106bcdf53198a55f5f5a
Roberto Chagas <55409496+RobertoChagas@users.noreply.github.com>
2022-07-09 19:07:50 -0300

    ajustes na imagem de fundo das do banner das páginas de olinda e manaus

--- frontend/src/components/CidadesNE-Olinda/BannerCidades/index.tsx
+++ frontend/src/components/CidadesNE-Olinda/BannerCidades/index.tsx
@@ -3,12 +3,16 @@ import Container from '../../Container';
 import Seta from "../../../assets/Images/chevron-down.svg";
 import Voltar from "../../../assets/Images/seta-voltar.png";
 import { Link } from 'react-router-dom'
+import background from '../../../assets/Images/Cidades/Olinda/capa-olinda.png';
 import './banner-cidades.css';
 
-const BannerCidades: React.FC = () => {
+const BannerOlinda: React.FC = () => {
     return (
         <Container>
-            <div className='banner-cidades'>
+            <div className='banner-cidades'  
+            style={{
+                backgroundImage:`url(${background})`
+              }}>
                 <Link to="/destinos/nordeste">
                     <img className='voltar-pagina' src={Voltar} alt="" />
                 </Link>
@@ -21,4 +25,4 @@ const BannerCidades: React.FC = () => {
     );
 }
 
-export default BannerCidades;
+export default BannerOlinda;
